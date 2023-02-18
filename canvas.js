fa4945332bc7eea64b35cf856a66b4bd715f09ab
gaoguang <13429883013@163.com>
2023-02-18 23:00:32 +0800

    fix error

--- canvas.js
+++ canvas.js
@@ -524,7 +524,9 @@ var canvas = (function(canvas) {
 			ctx.clearRect(0, 0, width, height);
 			
 			//cut Days to display
-			Days = Days.slice(Days.length - Configure.Days_Max_Show_lengh, Days.length);
+			var tmplengh = Days.length > Configure.Days_Max_lengh ? Configure.Days_Max_Show_lengh : 
+						Days.length - (Configure.Days_Max_lengh - Configure.Days_Max_Show_lengh);
+			Days = Days.slice(Days.length - tmplengh, Days.length);
 			drawSite(indecatorName, echelonNames);
 			drawIndicators(indecatorName, echelonNames);
 			drawEchelon(echelonNames);
